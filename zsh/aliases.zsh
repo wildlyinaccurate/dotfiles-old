53367572f91861004d67a4054773a615b424f645
Joseph Wynn <joseph_wynn@ipcmedia.com>
2013-10-14 12:01:54 +0100

    Add 'tags' alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,8 @@ alias reload!='. ~/.zshrc'
 
 alias dss='dig +search +short'
 
+alias tags='git tag | sort -bt. -k1,1 -k2,2n -k3,3n -k4,4n -k5,5n'
+
 alias svnst="svn st --ignore-externals | grep -v '^X'"
 alias svnlog='svn log --stop-on-copy'
 alias svnupdate='svn update --ignore-externals'
