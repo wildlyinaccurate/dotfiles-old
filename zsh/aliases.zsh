23ff11e778c0e1375709b8e60402c9fd93e45fa5
Joseph Wynn <joseph@wildlyinaccurate.com>
2014-10-27 17:21:45 +0000

    Add vagrant aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,8 @@ alias reload!='. ~/.zshrc'
 alias dss='dig +search +short'
 
 alias tags='git tag | sort -bt. -k1,1 -k2,2n -k3,3n -k4,4n -k5,5n'
+
+alias vu='vagrant up'
+alias vs='vagrant suspend'
+alias vr='vagrant reload'
+alias vp='vagrant provision'
